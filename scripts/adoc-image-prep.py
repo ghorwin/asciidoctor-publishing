78d45de2c5ad5114a76ff3cbd7c39d7955d7ada5
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-05-18 13:41:43 +0200

    Finished script.

--- scripts/adoc-image-prep.py
+++ scripts/adoc-image-prep.py
@@ -4,16 +4,25 @@
 # Image exchange tool to assist with asciidoctor publishing
 #
 # Syntax: adoc-image-prep.py <html|pdf> <path/to/adoc/files>
+#
+#
+# AsciiDoc syntax expectations:
+#
+# image: or image:: tags are followed by filename and either space, tab char or end of line. No spaces or [
+#                   are allowed in filename for now.
+
 
 import os
 import glob
 import sys
 import ntpath
 
+PRINT_FILE_SUFFIX = "-print"
+
 def processAdoc(fpath, mode, scriptPath):
 	try:
-		adocFilePath, adocFName = ntpath.split(fpath)
-		print("\nProcessing '{}' in '{}'".format(adocFName, adocFilePath))
+		adocDirPath, adocFName = ntpath.split(fpath)
+		print("\nProcessing '{}' in '{}'".format(adocFName, adocDirPath))
 		
 		# read the file
 		fobj = open(fpath, 'r')
@@ -28,7 +37,7 @@ def processAdoc(fpath, mode, scriptPath):
 			pos = line.find(":imagesdir:")
 			if pos != -1:
 				imagesdir = line[11:].strip()
-				imagesdir = os.path.abspath(os.path.join(adocFilePath, imagesdir))
+				imagesdir = os.path.abspath(os.path.join(adocDirPath, imagesdir))
 				print ("  Images dir = '{}'".format(imagesdir))
 				continue
 			# search for image: or image:: in text
@@ -52,10 +61,58 @@ def processAdoc(fpath, mode, scriptPath):
 				# pos2 now either holds the position of the first space, first '[' or -1 (end of line)
 				imagefname = line[pos:pos2].strip()
 				print("  Image ref = {}".format(imagefname))
-				# image tag is delimited by [ or space or tab
-				#print("'{}'".format(line[pos:pos2]))
-				pos = line.find("image:", pos2)
 				
+				# check for existing file
+				if len(imagesdir) != 0:
+					fullImagePath = os.path.join(imagesdir, imagefname)
+				else:
+					fullImagePath = os.path.join(adocDirPath, imagefname)
+				if not os.path.exists(fullImagePath):
+					raise RuntimeError("{}:{}:Image file {} ({}) not found".format(adocFName, i+1, imagefname, fullImagePath))
+
+				# now we check for -print suffix 
+				(basename,ext) = os.path.splitext(imagefname)
+				posPrint = basename.rfind(PRINT_FILE_SUFFIX)
+				# do we have a filename without 
+				if posPrint == -1:
+					htmlName = basename + ext
+					pdfName = basename + PRINT_FILE_SUFFIX + ext
+				else:
+					htmlName = basename[:-len(PRINT_FILE_SUFFIX)] + ext
+					pdfName = basename + ext
+				
+				# based on mode, determine target filename
+				if mode == "html":
+					targetFile = htmlName
+				else:
+					targetFile = pdfName
+				# check if it exists
+				if len(imagesdir) != 0:
+					fullTargetPath = os.path.join(imagesdir, targetFile)
+				else:
+					fullTargetPath = os.path.join(adocDirPath, targetFile)
+				if not os.path.exists(fullTargetPath):
+					print("  WARNING: Target file {} not found, keeping original file name".format(targetFile))
+					targetFile = imagefname
+				# now replace filenames
+				line = line[0:pos] + targetFile + line[pos2:]
+					
+				pos = line.find("image:", pos2)
+			# while loop end
+			
+			# store (potentially) modified line object
+			lines[i] = line
+			
+		# for loop end
+		
+		
+		# finally dump out the file again
+		fobj = open(fpath, 'w')
+		fobj.writelines(lines)
+		fobj.close()
+		del fobj
+		
+		
 	except IOError as e:
 		print(str(e))
 		raise RuntimeError("Error processing adoc file.")
